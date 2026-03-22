3df4468c49c8567e903f2a07374659831fb5aa1a
Shivam Dipte <shivamdipte0@gmail.com>
2026-03-22 22:42:55 +0530

    Pattern Tatat Dra the triangle Made of Alphabets

---
+++ alphabet_triangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class alphabet_triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter no of rows: ");
+        int n = sc.nextInt();
+        for(int i =0;i<=n;i++){
+            for(int j =1;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            //charecter
+            char ch ='A';
+            int breakpoint = (2*i+1)/2;
+            for(int j=1;j<=2*i+1;j++){
+                System.out.print(ch);
+                if(j<=breakpoint){
+                    ch++;
+                }
+                else{
+                    ch--;
+                }
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
